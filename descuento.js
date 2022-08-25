c7e0414478cf6cb881570c3dcdd7fb05e3322d56
Anthony Alvarado <cdotony17@gmail.com>
2022-08-25 01:37:06 -0500

    taller 2 avance

--- descuento.js
+++ descuento.js
@@ -1,12 +1,15 @@
-const precio = 150;
-const descuento = 20;
 
-const porcentajePrecioDescuento = 100 - descuento;
-const precioConDescuento = (precio * porcentajePrecioDescuento) / 100
+function calcularPrecioFinal(precio, descuento){
+    const porcentajePrecioDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioDescuento) / 100
+    return precioConDescuento;
+}
 
-console.log({
-    precio,
-    descuento,
-    porcentajePrecioDescuento,
-    precioConDescuento
-})
+function botonCalcularPrecio(preciohtml,descuentohtml){
+    const precioInicial = document.getElementById("Precio");
+    const precioIvalue = precioInicial.value;
+    const Descuento = document.getElementById("Descuento");
+    const Descuentovalue = Descuento.value;
+    precioDescuento = calcularPrecioFinal(precioIvalue,Descuentovalue);
+    alert("El precio final con descuento es: " +precioDescuento);
+}
