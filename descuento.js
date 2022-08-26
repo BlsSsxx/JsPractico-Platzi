f8b1b01787b33d233f620da317e85841393bf598
Anthony Alvarado <cdotony17@gmail.com>
2022-08-26 02:41:10 -0500

    terminado taller 2

--- descuento.js
+++ descuento.js
@@ -5,11 +5,35 @@ function calcularPrecioFinal(precio, descuento){
     return precioConDescuento;
 }
 
+const cupones = [
+    "invierno21",
+    "Jah",
+    "mjca"
+]
+
+
+
 function botonCalcularPrecio(preciohtml,descuentohtml){
     const precioInicial = document.getElementById("Precio");
     const precioIvalue = precioInicial.value;
-    const Descuento = document.getElementById("Descuento");
-    const Descuentovalue = Descuento.value;
-    precioDescuento = calcularPrecioFinal(precioIvalue,Descuentovalue);
-    alert("El precio final con descuento es: " +precioDescuento);
+    const Cupones = document.getElementById("Cupones");
+    const Cuponesvalue = Cupones.value;
+
+    let descuento;
+
+    if(Cuponesvalue === "invierno21"){
+        descuento = 15;
+    } else if(Cuponesvalue === "Jah"){
+        descuento = 20;
+    } else if(Cuponesvalue === "mjca"){
+        descuento = 35;
+    } else{
+        alert("El cupon " +Cuponesvalue +" no existe")
+    }
+
+    precioDescuento = calcularPrecioFinal(precioIvalue,descuento);
+
+    const resultadoP = document.getElementById("ResultadoPrecio");
+    resultadoP.innerText = "El precio final es: $" + precioDescuento + " con un " +descuento +"% de descuento";
 }
+
